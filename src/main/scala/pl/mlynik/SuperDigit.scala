1420e88fef80eb548ed7ae63bf40fd45d53d4f99
lmlynik <lukasz.mlynik@hotmail.com>
2015-07-06 19:43:15 +0200

    Removed magic numbers

--- src/main/scala/pl/mlynik/SuperDigit.scala
+++ src/main/scala/pl/mlynik/SuperDigit.scala
@@ -3,10 +3,12 @@ package pl.mlynik
 import scala.annotation.tailrec
 
 object SuperDigit {
+  val ASCII_DIGIT_OFFSET = 48
+  
   @tailrec
   def apply(number: Long): Long =
     if (number >= 10) apply(sumDigits(number)) else number
 
   private def sumDigits(number: Long): Long =
-    number.toString.foldLeft(0L)(_ + _.toInt - 48)
+    number.toString.foldLeft(0L)(_ + _.toInt - ASCII_DIGIT_OFFSET)
 }
